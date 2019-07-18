24ba7b2bfda447cb22b7de9e2c4d5f9be5e528af
aboulmagdApp <aboulmagd@live.com>
2019-07-18 17:12:46 +0300

    Tuples_Sets_Booleans in python

---
+++ pythonLevelOne/Tuples_Sets_Booleans.py
@@ -0,0 +1,27 @@
+#Tuples
+t =(1,2,3)
+mylist = [1,2,3]
+mylist[0] = 'New'
+print(mylist)
+
+# Sets are an unordered collection of *unique* elements.
+#SETS
+x = set()
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(3)
+x.add(3)
+print(x)
+
+
+mylist = [1,2,2,2,2,3,4,5,6,6,6,6,6,7,8,8,8,8,8,12,12,11,10]
+print(set(mylist))
+
+
+#Booleans
+
+a = True
+b = False
+print(1 > 2)
+print(1 < 2)
