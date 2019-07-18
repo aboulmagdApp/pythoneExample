f3643bbd09c60c89bce47678fce5d025f93e2d07
aboulmagdApp <aboulmagd@live.com>
2019-07-19 01:45:08 +0300

    Comparison-Logical_Operators

---
+++ pythonLevelOne/Comparison-Logical_Operators.py
@@ -0,0 +1,20 @@
+print(1 > 2)
+
+print(3 > 2)
+
+print(3 == 3)
+
+print(3 >= 2)
+
+print(2 <= 1)
+
+user = 'Admin'
+check = "Admin"
+
+print(user == check)
+
+print(1 == 1 and 2 < 3)
+
+print(1 == 2 and 2 < 3)
+
+print(1 == 2 or 2 < 3)
