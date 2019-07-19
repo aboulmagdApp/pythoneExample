b70de694d8e4daf50bcc38ec27b41503d04770af
aboulmagdApp <aboulmagd@live.com>
2019-07-20 01:44:24 +0300

    and method in class

--- Python_Level_Two/01-Object_Oriented_Programming.py
+++ Python_Level_Two/01-Object_Oriented_Programming.py
@@ -17,3 +17,17 @@ x = Dog('Huski','Sammy')
 print(x.breed)
 print(x.name)
 print(x.species)
+
+class circle():
+    pi=3.14
+    def __init__(self, radius=1):
+        self.radius = radius
+    def area(self):
+        return self.radius * self.radius * self.pi
+    def circumfrance(self):
+        return 2 * self.pi * self.radius
+
+mycircle = circle(10)
+print(mycircle.radius)
+print(mycircle.area())
+print(mycircle.circumfrance())
