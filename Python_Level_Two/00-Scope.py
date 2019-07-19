bf8b9a31d28adb6b83851f37cf22c157f1928246
aboulmagdApp <aboulmagd@live.com>
2019-07-20 00:19:27 +0300

    Scope  variable in Python

---
+++ Python_Level_Two/00-Scope.py
@@ -0,0 +1,31 @@
+x = 'outside'
+
+def report():
+    #Grap the global level of x
+    global x
+    print(x)
+    #Reassign global variable
+    x = 'inside'
+    return x
+
+print(report())
+print(x)
+
+#Local
+def report():
+    #local Assignment
+    x = 'local'
+    print(x)
+
+#Enclosing
+z = 'is the global level'
+
+def enclosing():
+   # z = 'Enclosing Level'
+
+    def inside():
+        print(z)
+
+    inside()
+
+enclosing()
