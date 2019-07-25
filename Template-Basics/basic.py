6df508f631f8ad98e3839bc992998ec76f6e9008
aboulmagdApp <aboulmagd@live.com>
2019-07-25 14:33:23 +0300

    Template Variables

--- Template-Basics/basic.py
+++ Template-Basics/basic.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    name = "aboulmagd"
+    letters = list(name)
+    pup_dictionary = {'pup_name':'sammy'}
+    return render_template('basic.html',name=name,letters=letters,pup_dictionary=pup_dictionary)
 
 if __name__ == '__main__':
     app.run(debug=True)
