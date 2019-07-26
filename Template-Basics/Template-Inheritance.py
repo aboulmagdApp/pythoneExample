c981359acf0b2c454fe5c5bf10f2b69f7740ccbe
aboulmagdApp <aboulmagd@live.com>
2019-07-26 19:07:28 +0300

    Template Forms

--- Template-Basics/Template-Inheritance.py
+++ Template-Basics/Template-Inheritance.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('home.html')
+    return render_template('index.html')
+
+# This page will have the sign up form
+@app.route('/signup_form')
+def signup_form():
+    return render_template('Sign-up.html')
+
+# This page will be the page after the form
+@app.route('/thank_you')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html',first=first,last=last)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 @app.route('/puppy/<name>')
 def pup_name(name):
